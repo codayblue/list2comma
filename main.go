59e1cefa8028211997a75a4636b9ede46d15696d
codayblue <codayblue@gmail.com>
2022-01-29 11:56:27 -0800

    version flag caused issues

--- main.go
+++ main.go
@@ -7,14 +7,6 @@ import (
 	"strings"
 )
 
-var commit string = ""
-var version string = "dev"
-
-func printVersion() {
-	fmt.Printf("Version: %s\n", version)
-	fmt.Printf("Git SHA: %s\n", commit)
-}
-
 func list2comma(stdin *os.File) (string, error) {
 	scanner := bufio.NewScanner(stdin)
 	lines := []string{}
@@ -31,16 +23,10 @@ func list2comma(stdin *os.File) (string, error) {
 }
 
 func main() {
-
-	if os.Args[1] == "version" {
-		printVersion()
-		os.Exit(0)
-	}
-
 	result, err := list2comma(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(result)
+	fmt.Print(result)
 }
