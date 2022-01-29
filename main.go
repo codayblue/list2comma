c81617acfc40a0e1e466cf021f8ced7da9622509
codayblue <codayblue@gmail.com>
2022-01-29 11:52:29 -0800

    Release ready

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	"strings"
 )
 
+var commit string = ""
+var version string = "dev"
+
+func printVersion() {
+	fmt.Printf("Version: %s\n", version)
+	fmt.Printf("Git SHA: %s\n", commit)
+}
+
 func list2comma(stdin *os.File) (string, error) {
 	scanner := bufio.NewScanner(stdin)
 	lines := []string{}
@@ -23,6 +31,12 @@ func list2comma(stdin *os.File) (string, error) {
 }
 
 func main() {
+
+	if os.Args[1] == "version" {
+		printVersion()
+		os.Exit(0)
+	}
+
 	result, err := list2comma(os.Stdin)
 	if err != nil {
 		panic(err)
